c09833c67be495d062b9df3859a8ddee3c21852a
wenxin.leong <wenxin.leong@infineon.com>
2024-07-17 09:25:34 +0800

    [server] create an empty project
    Signed-off-by: wenxin.leong <wenxin.leong@infineon.com>

---
+++ server/provision/src/main/java/com/infineon/tpm2/provision/ProvisionApplication.java
@@ -0,0 +1,13 @@
+package com.infineon.tpm2.provision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvisionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProvisionApplication.class, args);
+	}
+
+}
